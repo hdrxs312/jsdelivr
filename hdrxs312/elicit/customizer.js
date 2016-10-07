a902af65d25a9ffd10c4a2a6bf8d6551ae706b6e
hdrxs312 <hdrxs312@gmail.com>
2016-10-07 20:14:35 +0700

    Create customizer.js

---
+++ hdrxs312/elicit/customizer.js
@@ -0,0 +1,4 @@
+
+jQuery( document ).ready( function( $ ){
+    $('#customize-info .accordion-section-title').append('<a target="_blank" style="text-transform: uppercase; background: #005695; color: #fff; font-size: 16px; line-height: 14px; padding: 10px; display: inline-block;" href="https://mizmizi.com/themes/elicit-pro">UPGRADE TO PRO</a>');
+} );
